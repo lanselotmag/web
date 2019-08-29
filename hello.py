e52a85c9bd60cbf434d315b245c212c635017f89
lanselotmag <lanselot@land.ru>
2019-08-29 07:37:07 +0000

    Final ver of nginx+unicorn

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 
 def app(env,start_response):
 	start_response('200 OK',[('Content-type','text/plain')])
-	body = ['\r\n'.join(env['QUERY_STRING'].split('&'))]
-	#body =  [bytes(i+'\n',ascii) for i in env['QUERY_STRING'].split('&')]
+	#body = ['\r\n'.join(env['QUERY_STRING'].split('&'))]
+	body =  [bytes(i+'\n','ascii') for i in env['QUERY_STRING'].split('&')]
 	return body
