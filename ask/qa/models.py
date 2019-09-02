470235bd3beb7abc47d6444e4dcc1784b53cc542
lanselotmag <la@la.ld>
2019-09-02 10:27:15 +0000

    not working model

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,10 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-from django.contrib.auth.models.User import User
+from django.contrib.auth.models import User
 
 # Create your models here.
-class QuestionManager(self):
+class QuestionManager(models.Manager):
 	def new(self):
 		return self.order_by('-added_at')
 	def popular(self):
@@ -16,19 +16,19 @@ class Question(models.Model):
 	objects=QuestionManager()
 	title=models.CharField(default="",max_length=255)
 	text=models.TextField(default="")
-	added_at=models.DataField(auto_now_add=true)
+	added_at=models.DateField(auto_now_add=True)
 	rating=models.IntegerField(default=0)
 	author=models.OneToOneField(User)
-	like=models.OneToManyField(User)
+	likes=models.ForeignKey(User,null=True,on_delete=models.SET_NULL)
 
 	def __str__(self):
 		return self.title
 
 class Answer(models.Model):
 	text=models.CharField(max_length=255)
-	added_at=models.DataField()
-	question=models.OneToOneField()
-	author=models.OneToOneField()
+	added_at=models.DateField(auto_now_add=True)
+	question=models.OneToOneField(Question)
+	author=models.OneToOneField(User)
 
 	def __str__(self):
 		return self.title
