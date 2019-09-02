86c8889a7c1e2b73c22ba72afeec8ad50436b0ae
lanselotmag <lanselot@land.ru>
2019-09-02 10:40:46 +0000

    second model test

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
-
 from django.contrib.auth.models import User
-
 # Create your models here.
 class QuestionManager(models.Manager):
 	def new(self):
 		return self.order_by('-added_at')
+
 	def popular(self):
 		return self.order_by('-rating')
 
 class Question(models.Model):
 	objects=QuestionManager()
+
 	title=models.CharField(default="",max_length=255)
 	text=models.TextField(default="")
 	added_at=models.DateField(auto_now_add=True)
