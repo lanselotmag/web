f66c43a7b15b7df99db6f4504c8a81005f3fcc7b
lanselotmag <lanselot@land.ru>
2019-09-03 12:10:41 +0000

    Question test

--- init.sh
+++ init.sh
@@ -9,8 +9,8 @@
 #sudo service nginx restart
 #pip install pymysql
 sudo /etc/init.d/mysql start
-#sudo mysql -uroot -e "CREATE DATABASE stepik;"
-#sudo mysql -uroot -e "CREATE USER 'boxdb'@'localhost';"
-#sudo mysql -uroot -e "SET PASSWORD FOR 'boxdb'@'localhost'=PASSWORD('password1');"
-#sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON stepik.* TO 'boxdb'@'localhost';"
-#sudo mysql -uroot -e "FLUSH PRIVILEGES;"
+sudo mysql -uroot -e "CREATE DATABASE stepik;"
+sudo mysql -uroot -e "CREATE USER 'box'@'localhost';"
+sudo mysql -uroot -e "SET PASSWORD FOR 'box'@'localhost'=PASSWORD('password1');"
+sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'box'@'localhost';"
+sudo mysql -uroot -e "FLUSH PRIVILEGES;"
