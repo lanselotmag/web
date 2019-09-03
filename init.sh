a8890723b6c148522db78c88f52326879ec9eb50
lanselotmag <lanselot@land.ru>
2019-09-03 04:20:11 +0000

    config mysql in init.sh

--- init.sh
+++ init.sh
@@ -7,3 +7,9 @@
 sudo gunicorn -c ~/web/etc/django.py ask.wsgi:appplication -D
 sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo service nginx restart
+
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE stepik CHARACTER SET utf8mb64 COLLATE utf8mb64_unicode_sci;"
+mysql -uroot -e "CREATE 'box'@'localhost' IDENTIFIED BY 'password1';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stepik.* TO 'box'@'localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
