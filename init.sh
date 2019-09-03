e2331ad25421013a11f063bc7c8921f986e37f80
lanselotmag <lanselot@land.ru>
2019-09-03 09:49:45 +0000

    New model test

--- init.sh
+++ init.sh
@@ -7,9 +7,10 @@
 #sudo gunicorn -c ~/web/etc/django.py ask.wsgi:appplication -D
 #sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 #sudo service nginx restart
-
+#pip install pymysql
 sudo /etc/init.d/mysql start
-mysql -uroot -e "CREATE DATABASE stepik CHARACTER SET utf8mb64 COLLATE utf8mb64_unicode_sci;"
-mysql -uroot -e "CREATE 'box'@'localhost' IDENTIFIED BY 'password1';"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON stepik.* TO 'box'@'localhost';"
-mysql -uroot -e "FLUSH PRIVILEGES;"
+#sudo mysql -uroot -e "CREATE DATABASE stepik;"
+#sudo mysql -uroot -e "CREATE USER 'boxdb'@'localhost';"
+#sudo mysql -uroot -e "SET PASSWORD FOR 'boxdb'@'localhost'=PASSWORD('password1');"
+#sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON stepik.* TO 'boxdb'@'localhost';"
+#sudo mysql -uroot -e "FLUSH PRIVILEGES;"
