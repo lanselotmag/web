24f9fe6209747bc8c1f48ec7f088806e796e5a9d
lanselotmag <la@la.la>
2019-09-03 05:10:47 +0000

    coonection error

--- init.sh
+++ init.sh
@@ -4,9 +4,9 @@
 #sudo ln -sf ~/web/etc/hello.py /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 #sudo ln -sf ~/web/etc/django.py /etc/gunicorn.d/django.py
-sudo gunicorn -c ~/web/etc/django.py ask.wsgi:appplication -D
-sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo service nginx restart
+#sudo gunicorn -c ~/web/etc/django.py ask.wsgi:appplication -D
+#sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+#sudo service nginx restart
 
 sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE stepik CHARACTER SET utf8mb64 COLLATE utf8mb64_unicode_sci;"
